4b66c3cc956ab1539c4c40f60260114fd3f0966d
Buck Baskin <mobile.wbaskin@gmail.com>
2018-05-24 12:42:14 -0400

    Define McCree. Need ammo and reload times

---
+++ overplay/mccree.py
@@ -0,0 +1,20 @@
+from overplay import Hero, Ability
+
+flashbang = Ability(damage=25, healing=0.0, casttime=0.5, duration=0.7,
+cooldown=10, has_stun=True, has_knockback=False, has_falloff=False, 
+start_falloff=0.0, end_falloff=0.0, fallen_damage=0.0)
+
+primary = Ability(damage=70, healing=0.0, casttime=0.0, duration = 0.0,
+cooldown=1.0/2, has_falloff=True, start_falloff=22, end_falloff=45,
+fallen_damage=22)
+
+fth = Ability(damage=70*6, healing=0.0, casttime=0.0, duration=6.0/5, cooldown =
+8, has_falloff=True, start_falloff=22, end_falloff=45, fallen_damage=22)
+
+abilities = {
+   'primary': primary,
+   'fth': fth,
+   'flashbang': flashbang,
+}
+
+McCree = Hero(health=200, **abilities)
